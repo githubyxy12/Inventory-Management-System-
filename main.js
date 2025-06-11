c12c97a1bf4c33779926370f38704d3f98524c62
githubyxy12 <91432503+githubyxy12@users.noreply.github.com>
2025-06-11 19:50:14 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("=========================================");
+console.log("     Welcome to Inventory Management     ");
+console.log("                 System                  ");
+console.log("=========================================\n");
